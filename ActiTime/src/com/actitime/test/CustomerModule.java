74cbff481f0913d6f21b249b127069b44bc14c96
Admin <Admin@Admin-PC>
2020-01-25 10:47:28 +0530

    first commit

---
+++ ActiTime/src/com/actitime/test/CustomerModule.java
@@ -0,0 +1,45 @@
+package com.actitime.test;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.actitime.generics.BaseClass;
+import com.actitime.pom.EnterTimeTrack;
+import com.actitime.pom.TaskListPage;
+/**
+ * 
+ * @author sandeep
+ *
+ */
+public class CustomerModule extends BaseClass{
+	
+@Test
+public void testCreateCustome() throws InterruptedException, Exception, InvalidFormatException, IOException {
+	Reporter.log("testCreateCustome",true);
+	EnterTimeTrack e=new EnterTimeTrack(driver);
+	TaskListPage t=new TaskListPage(driver);
+	String CustomerName = f.getExcelValue("CreateCustomer", 1, 0);
+	String CustomerDesc = f.getExcelValue("CreateCustomer", 1, 1);
+	Thread.sleep(3000);
+	e.taskTabClick();
+	Thread.sleep(2000);
+	t.getAddNewBTN().click();
+	t.getNewCustBTN().click();
+	Thread.sleep(3000);
+	t.getEnterCustTBX().sendKeys(CustomerName);
+	t.getDescriptionTBX().sendKeys(CustomerDesc);
+	Thread.sleep(3000);
+	t.getSelectCustListBX().click();
+	Thread.sleep(2000);
+	t.getProjectName().click();
+	t.getCreateCustBTN().click();
+	String Atext = t.getActualText().getText();
+	Assert.assertEquals(Atext, CustomerName);
+	Thread.sleep(2000);
+}
+}
